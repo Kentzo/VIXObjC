d9f193a88590dc4d4ec4f72b35786a9ab261516f
Ilya Kulakov <kulakov.ilya@gmail.com>
2012-10-28 17:53:36 +0700

    Implement properties for the VM.

--- VIXObjC/VIXVirtualMachine.h
+++ VIXObjC/VIXVirtualMachine.h
@@ -20,4 +20,69 @@
 - (instancetype)initWithHost:(VIXHost *)aHost
               nativeVMHandle:(VixHandle)aNativeVMHandle;
 
+/*!
+    @brief  Number of available processors. nil if property is not applicable.
+ */
+- (NSNumber *)numberOfProcessors;
+
+/*!
+    @brief  Available memory size in Mbytes. nil if property is not applicable.
+ */
+- (NSNumber *)memorySize;
+
+/*!
+    @brief  Power state of the VM. nil if property is not applicable.
+    @see
+ */
+- (NSNumber *)powerState;
+
+/*!
+    @brief  VMware Tools suite state of the VM. nil if property is not applicable.
+    @see    VixToolsState
+ */
+- (NSNumber *)toolsState;
+
+/*!
+    @brief  Indicates whether VM is running. nil if property is not applicable.
+ */
+- (NSNumber *)isRunning;
+
+/*!
+    @brief  Indicates whether VM is read only. nil if property is not applicable.
+ */
+- (NSNumber *)isReadOnly;
+
+/*!
+    @brief  Name of the VM. nil if property is not applicable.
+ */
+- (NSString *)name;
+
+/*!
+    @brief  Path to the VM configuration file (.vmx). nil if property is not applicable.
+ */
+- (NSString *)path;
+
+/*!
+    @brief  Indicates whether VM is part of team. nil if property is not applicable.
+ */
+- (NSNumber *)isPartOfTeam;
+
+/*!
+    @brief  Path to the VM team. nil if property is not applicable.
+ */
+- (NSString *)teamPath;
+
+/*!
+    @brief  Codename of the guestOS. nil if property is not applicable.
+ */
+- (NSString *)guestOS;
+
+/*!
+    @brief  SSL Error. nil if property is not applicable.
+    @see    VIXHost
+ */
+- (NSNumber *)sslError;
+
+- (NSNumber *)supportedFeatures;
+
 @end
--- VIXObjC/VIXVirtualMachine.m
+++ VIXObjC/VIXVirtualMachine.m
@@ -33,4 +33,154 @@
     Vix_ReleaseHandle(_handle);
 }
 
+
+#pragma mark Properties
+
+static NSString* GetStringProperty(VixHandle aVMHandle, VixPropertyID aPropertyID)
+{
+    VixPropertyType type = VIX_PROPERTYTYPE_ANY;
+    VixError error = Vix_GetPropertyType(aVMHandle,
+                                         aPropertyID,
+                                         &type);
+
+    if (VIX_SUCCEEDED(error) && type == VIX_PROPERTYTYPE_STRING)
+    {
+        char *string = NULL;
+        error = Vix_GetProperties(aVMHandle,
+                                  aPropertyID, &string,
+                                  VIX_PROPERTY_NONE);
+        NSString *objcString = nil;
+
+        if (VIX_SUCCEEDED(error))
+            objcString = @(string);
+
+        Vix_FreeBuffer(string);
+        return objcString;
+    }
+    else
+        return nil;
+}
+
+static NSNumber* GetNumberProperty(VixHandle aVMHandle, VixPropertyID aPropertyID)
+{
+    VixPropertyType type = VIX_PROPERTYTYPE_ANY;
+    VixError error = Vix_GetPropertyType(aVMHandle,
+                                         aPropertyID,
+                                         &type);
+
+    if (VIX_SUCCEEDED(error))
+    {
+        switch (type)
+        {
+            case VIX_PROPERTYTYPE_INTEGER:
+            {
+                int value = 0;
+                error = Vix_GetProperties(aVMHandle,
+                                          aPropertyID, &value,
+                                          VIX_PROPERTY_NONE);
+
+                if (VIX_SUCCEEDED(error))
+                    return @(value);
+                else
+                    return nil;
+            }
+            case VIX_PROPERTYTYPE_INT64:
+            {
+                long long value = 0;
+                error = Vix_GetProperties(aVMHandle,
+                                          aPropertyID, &value,
+                                          VIX_PROPERTY_NONE);
+
+                if (VIX_SUCCEEDED(error))
+                    return @(value);
+                else
+                    return nil;
+            }
+            case VIX_PROPERTYTYPE_BOOL:
+            {
+                Bool value = 0;
+                error = Vix_GetProperties(aVMHandle,
+                                          aPropertyID, &value,
+                                          VIX_PROPERTY_NONE);
+
+                if (VIX_SUCCEEDED(error))
+                    return @(value);
+                else
+                    return nil;
+            }
+            default:
+                return nil;
+        }
+    }
+    else
+        return nil;
+}
+
+- (NSNumber *)numberOfProcessors
+{
+    return GetNumberProperty(_handle, VIX_PROPERTY_VM_NUM_VCPUS);
+}
+
+- (NSString *)path
+{
+    return GetStringProperty(_handle, VIX_PROPERTY_VM_VMX_PATHNAME);
+}
+
+- (NSString *)teamPath
+{
+    return GetStringProperty(_handle, VIX_PROPERTY_VM_VMTEAM_PATHNAME);
+}
+
+- (NSNumber *)memorySize
+{
+    return GetNumberProperty(_handle, VIX_PROPERTY_VM_MEMORY_SIZE);
+}
+
+- (NSNumber *)isReadOnly
+{
+    return GetNumberProperty(_handle, VIX_PROPERTY_VM_READ_ONLY);
+}
+
+- (NSString *)name
+{
+    return GetStringProperty(_handle, VIX_PROPERTY_VM_NAME);
+}
+
+- (NSString *)guestOS
+{
+    return GetStringProperty(_handle, VIX_PROPERTY_VM_GUESTOS);
+
+}
+
+- (NSNumber *)sslError
+{
+    return GetNumberProperty(_handle, VIX_PROPERTY_VM_SSL_ERROR);
+}
+
+- (NSNumber *)supportedFeatures
+{
+    return GetNumberProperty(_handle, VIX_PROPERTY_VM_SUPPORTED_FEATURES);
+}
+
+
+- (NSNumber *)isPartOfTeam
+{
+    return GetNumberProperty(_handle, VIX_PROPERTY_VM_IN_VMTEAM);
+}
+
+- (NSNumber *)powerState
+{
+    return GetNumberProperty(_handle, VIX_PROPERTY_VM_POWER_STATE);
+}
+
+- (NSNumber *)toolsState
+{
+    return GetNumberProperty(_handle, VIX_PROPERTY_VM_TOOLS_STATE);
+}
+
+- (NSNumber *)isRunning
+{
+    return GetNumberProperty(_handle, VIX_PROPERTY_VM_IS_RUNNING);
+}
+
 @end
